9a4c55615c484e49792e7028c348a4f621b5cd86
yueyue <yueerbrilliant@gmail.com>
2018-07-06 21:21:13 +0800

    push

---
+++ script/util.js
@@ -0,0 +1,4 @@
+var connectUrl ="http://47.106.213.57:8080/firefighting/";
+//var connectUrl ="http://192.168.0.7:8080/firefighting/";
+// 'http://192.168.0.7:8080/firefighting/',
+$api.setStorage('connectUrl', connectUrl);
